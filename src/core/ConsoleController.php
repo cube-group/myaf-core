69ac3ee0453af929a9f1fd77163e25b9050b597e
linyang <lin2798003@sina.com>
2018-06-04 16:43:38 +0800

    优化ConsoleController

--- src/core/ConsoleController.php
+++ src/core/ConsoleController.php
@@ -19,11 +19,30 @@ abstract class ConsoleController extends Controller
         }
     }
 
+    /**
+     * 获取console模式下命令行获取到的参数
+     * @return string|int|array
+     */
+    protected function params()
+    {
+        return $this->getRequest()->getParam('value');
+    }
+
+    /**
+     * 直接打印信息
+     * @param array|string $value
+     */
     public function send($value)
     {
         echo $value . "\n";
     }
 
+    /**
+     * 直接打印json信息
+     * @param string $data
+     * @param string $msg
+     * @param int $code
+     */
     public function json($data = '', $msg = '', $code = 0)
     {
         echo G::json($data, $msg, $code) . "\n";
